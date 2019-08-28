50c773a3db10a97a01524a92887cbc57994f1706
freisoncastro@gmail.com <freisoncastro@gmail.com>
2019-08-28 15:01:16 -0600

    First.
    First.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('#btnReserve').click(function () {
+        $('#exampleModalLong').modal('show');
+    })
+    $('#btnLogin').click(function () {
+        $('#lgmodal').modal('show');
+    })
+});
